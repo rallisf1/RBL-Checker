e5fd49bea1ffaef4509eb1e33ef2803e1fb9c184
thodoris <thodoriskoutouritos@gmail.com>
2026-01-19 10:35:57 +0200

    fixed buttons

--- src/routes/ips/[id]/+page.svelte
+++ src/routes/ips/[id]/+page.svelte
@@ -1,39 +1,46 @@
 <script lang="ts">
-import type { PageProps } from './$types';
-import { enhance, applyAction } from '$app/forms'
-import Datatable from "$lib/components/Datatable.svelte";   
-import ModalMany from '$lib/components/ModalMany.svelte';
-import { showToast } from '$lib/Toast'
+	import type { PageProps } from './$types';
+	import { enhance, applyAction } from '$app/forms';
+	import Datatable from '$lib/components/Datatable.svelte';
+	import ModalMany from '$lib/components/ModalMany.svelte';
+	import { showToast } from '$lib/Toast';
 	import Input from '$lib/components/Input.svelte';
 	import { goto } from '$app/navigation';
-let { data }: PageProps = $props();
+	let { data }: PageProps = $props();
 </script>
 
-
-<form method="POST" action="?/create" use:enhance={() => {
-    return async ({ result, update }) => {
-        if (result.type === 'success') {
-            await update()
-            goto('/ips')
-        } else {
-            await applyAction(result)
-        }
-    }
-}}>
-    <div class="flex gap-4">
-        <Input labels={["Please enter ip",null,null,null]} name="ip" minlength={3} required />
-        <Input labels={["Please enter notes",null,null,null]} name="notes" minlength={3} required />
-    </div>
-    <div class="modal-footer">
-        <button class="btn btn-primary" type="submit" onclick={() => {  
-            showToast({
-                type: 'success',
-                message: 'Record Created',
-                duration: 100000,
-                dismissible: true,
-                positionX: 'right',
-                positionY: 'top'
-            })
-        }}>Submit</button>
-    </div>
+<form
+	method="POST"
+	action="?/create"
+	use:enhance={() => {
+		return async ({ result, update }) => {
+			if (result.type === 'success') {
+				await update();
+				goto('/ips');
+			} else {
+				await applyAction(result);
+			}
+		};
+	}}
+>
+	<div class="flex gap-4">
+		<Input labels={['Please enter ip', null, null, null]} name="ip" minlength={3} required />
+		<Input labels={['Please enter notes', null, null, null]} name="notes" minlength={3} required />
+	</div>
+	<div class="modal-footer">
+		<button
+			class="btn btn-primary"
+			type="submit"
+			onclick={() => {
+				showToast({
+					type: 'success',
+					message: 'Record Created',
+					duration: 100000,
+					dismissible: true,
+					positionX: 'right',
+					positionY: 'top'
+				});
+			}}>Submit</button
+		>
+	</div>
 </form>
--- src/routes/rbls/+page.svelte
+++ src/routes/rbls/+page.svelte
@@ -1,61 +1,80 @@
 <script lang="ts">
-import type { PageProps } from './$types';
-import { enhance, applyAction } from '$app/forms'
-import Datatable from "$lib/components/Datatable.svelte";   
-import ModalMany from '$lib/components/ModalMany.svelte';
-import { showToast } from '$lib/Toast'
-let { data }: PageProps = $props();
-let deleteModal = $state(false)
-let modalIndex: string|number|undefined = $state()
+	import type { PageProps } from './$types';
+	import { enhance, applyAction } from '$app/forms';
+	import Datatable from '$lib/components/Datatable.svelte';
+	import ModalMany from '$lib/components/ModalMany.svelte';
+	import { showToast } from '$lib/Toast';
+	let { data }: PageProps = $props();
+	let deleteModal = $state(false);
+	let modalIndex: string | number | undefined = $state();
 
-$effect(() => {
-	console.log(data);
-});
-
+	$effect(() => {
+		console.log(data);
+	});
 </script>
 
 <div class="flex justify-between items-center">
-    <h1 class="text-base-content text-4xl m-4 ml-0 flex">
-        rbls
-    </h1>
-    <a href={data.activeMenu!.link + "/new"} title="Add a new rbls" class="btn btn-primary btn-circle">+</a>
-    
+	<h1 class="text-base-content text-4xl m-4 ml-0 flex">rbls</h1>
+	<a href={data.activeMenu!.link + '/new'} title="Add a new rbls" class="btn btn-primary btn-circle"
+		>+</a
+	>
 </div>
 <Datatable
-    Rows={data.records}
-    headers={["id","name", "domain", "disabled", "delist","Actions"]}
-    action={true}
-    onDeleteClick={(recordId) => {modalIndex = recordId; deleteModal = true }} EditRedirect={`/rbls/${modalIndex}`}  onEditClick={(recordId) => {modalIndex = recordId}}
-    >
-</Datatable>
+	Rows={data.records}
+	headers={['id', 'name', 'domain', 'disabled', 'delist', 'Actions']}
+	action={true}
+	onDeleteClick={(recordId) => {
+		modalIndex = recordId;
+		deleteModal = true;
+	}}
+	EditRedirect={(id) => `/rbls/${id}`}
+	onEditClick={(recordId) => {
+		modalIndex = recordId;
+	}}
+></Datatable>
 
-<ModalMany position="middle" size="lg" Id="deleteModal" title="Delete Row" open={deleteModal} close={() => deleteModal = false} recordId={modalIndex}>
-    {#snippet Body(recordId: string|number|undefined)}
-        <h1>Are you sure you want to delete this row</h1>
-        <form method="POST" action="?/delete" use:enhance={({}) => {
-                return async ({ result,update }) => {
-                    if (result.type === 'success') {
-                        await update()
-                    } else {
-                        await applyAction(result)
-                    }
-                }
-            }}
-            >
-            <input type="hidden" name="recordId" value={recordId} />
-            <div class="modal-footer">
-                <button class="btn btn-primary" type="submit" onclick={() => { 
-                deleteModal = false; 
-                showToast({
-                    type: 'success',
-                    message: 'Record Deleted',
-                    duration: 100000,
-                    dismissible: true,
-                    positionX: 'right',
-                    positionY: 'top'
-                }); 
-            }}>Submit</button>
-            </div>
-        </form>
-    {/snippet}
+<ModalMany
+	position="middle"
+	size="lg"
+	Id="deleteModal"
+	title="Delete Row"
+	open={deleteModal}
+	close={() => (deleteModal = false)}
+	recordId={modalIndex}
+>
+	{#snippet Body(recordId: string | number | undefined)}
+		<h1>Are you sure you want to delete this row</h1>
+		<form
+			method="POST"
+			action="?/delete"
+			use:enhance={({}) => {
+				return async ({ result, update }) => {
+					if (result.type === 'success') {
+						await update();
+					} else {
+						await applyAction(result);
+					}
+				};
+			}}
+		>
+			<input type="hidden" name="recordId" value={recordId} />
+			<div class="modal-footer">
+				<button
+					class="btn btn-primary"
+					type="submit"
+					onclick={() => {
+						deleteModal = false;
+						showToast({
+							type: 'success',
+							message: 'Record Deleted',
+							duration: 100000,
+							dismissible: true,
+							positionX: 'right',
+							positionY: 'top'
+						});
+					}}>Submit</button
+				>
+			</div>
+		</form>
+	{/snippet}
 </ModalMany>
